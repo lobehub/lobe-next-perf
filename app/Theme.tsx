87ffec10b068fd48256d018f63a334525825d1e3
Arvin Xu <arvinx@foxmail.com>
2024-10-22 09:52:37 +0000

    refactor switch

--- app/Theme.tsx
+++ app/Theme.tsx
@@ -3,28 +3,12 @@
 import { ThemeAppearance, ThemeProvider } from 'antd-style';
 import { useState } from 'react';
 
-import { Segmented, Divider } from 'antd';
-
 import StyleRegistry from './StyleRegistry';
 
-const options = [
-  { label: '亮色', value: 'light' },
-  { label: '暗色', value: 'dark' },
-];
-
 const Theme = ({ children }:any) => {
-  const [appearance, setTheme] = useState<ThemeAppearance>('light');
-
   return (
     <StyleRegistry>
-      <ThemeProvider theme={{ cssVar: true }} appearance={appearance}>
-      <div>
-        <Segmented
-          onChange={(v) => setTheme(v as ThemeAppearance)}
-          options={options}
-        />
-      </div>
-      <Divider />
+      <ThemeProvider theme={{ cssVar: true }}>
         {children}
       </ThemeProvider>
     </StyleRegistry>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,26 @@
-'use client';
+"use client";
 
-import { Button } from 'antd';
-import Theme from './Theme';
-import { useTheme } from 'antd-style';
+import { Button } from "antd";
+import Theme from "./Theme";
+import { useTheme, useThemeMode } from "antd-style";
+
+const options = [
+  { label: "亮色", value: "light" },
+  { label: "暗色", value: "dark" },
+];
 
 export default function Home() {
   const theme = useTheme();
+  const { appearance, setAppearance } = useThemeMode();
   return (
-    <div style={{ background: theme.colorBgLayout, height: '90vh' }}>
+    <div style={{ background: theme.colorBgLayout, height: "90vh" }}>
+      <div>
+        <Segmented
+          onChange={(v) => setAppearance(v as ThemeAppearance)}
+          options={options}
+        />
+      </div>
+      <Divider />
       <div>
         <Button>按钮</Button>
       </div>
